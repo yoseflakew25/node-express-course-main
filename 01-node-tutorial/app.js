410f78311f6b067691d3f009f3c1da1f9c4453db
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-24 23:07:42 +0300

    .

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,1 +1,11 @@
-console.log('Welcome to Node Tutorial')
+const amount = 12
+
+
+if (amount < 10) {
+    console.log("Small number");
+} else {
+    console.log("Large number");
+}
+
+
+console.log("hello world my name is yosef lakew i am a software engineering student");
