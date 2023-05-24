e9663343410e3d30e3bf237c7ec38fb143034a48
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-24 23:17:07 +0300

    .

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,11 +1,1 @@
-const amount = 12
-
-
-if (amount < 10) {
-    console.log("Small number");
-} else {
-    console.log("Large number");
-}
-
-
-console.log("hello world my name is yosef lakew i am a software engineering student");
+// Modules
