a616999cee4b58be476d744f179e0821e5ec5275
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-26 00:54:31 +0300

    .

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,13 +1,1 @@
-const path = require('path');
-
-
-const pathFile = path.join(__dirname, 'content', 'subfolder', 'test.txt');
-
-
-console.log(pathFile);
-console.log(path.basename(pathFile));
-
-
-
-const absolute = path.resolve
-(__dirname, 'content', 'subfolder', 'test.txt');
+const { readFileSync, writeFileSync } = require('fs')
