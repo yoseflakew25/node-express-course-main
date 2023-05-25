030f0e299cfe45df9ac1d05b546c0fce1b438ab6
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-25 13:10:41 +0300

    .

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -5,3 +5,9 @@ const pathFile = path.join(__dirname, 'content', 'subfolder', 'test.txt');
 
 
 console.log(pathFile);
+console.log(path.basename(pathFile));
+
+
+
+const absolute = path.resolve
+(__dirname, 'content', 'subfolder', 'test.txt');
