ade3623b038c25dd282d970d68966ccce14ade3b
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-25 10:19:45 +0300

    .

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,8 +1,10 @@
 // Modules
 
 const names = require('./04-names')
+const data = require('./06-alternative-flavor')
+const sayHi = require('./05-utils')
 
-console.log(names);
+// console.log(names);
 
 // const sayHi = (name) => {
 
@@ -11,9 +13,11 @@ console.log(names);
 
 // }
 
+console.log(data.singlePerson.name);
 
+sayHi(data.singlePerson.name)
 
-// sayHi("yosef")
+// sayHi(names.john)
 
 // sayHi(john)
 
