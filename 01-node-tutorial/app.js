bc63cb171d66a4740c8b36b3151920be218acae5
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-26 01:12:59 +0300

    .

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,16 +1,17 @@
-const { readFileSync, writeFileSync } = require('fs')
-
-
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
-
-const result = readFileSync('./content/result-sync.txt', 'utf8')
-writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${second}`, { flag: 'a' })
+const { readFile, writeFile } = require('fs');
 
 
 
+const first=''
+readFile('./content/first.txt', 'utf8', (err, result) => { 
+    if (err) {
+        console.log(err);
+        return;
+    }
+    first = result
+})
 
-// console.log(first);
-// console.log(second);
 
-console.log(result);
+console.log(first);
+
+
