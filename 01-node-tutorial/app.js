c96d8da70798862a871db4c724afc83f3cbe2d65
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-26 01:36:29 +0300

    .

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,20 +1,2 @@
-const http = require('http');
-
-
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('wellcome to our home page')
-    }
-
-    if (req.url==='/about') {
-        res.end('this is our about page')
-    }
-
-
-    res.end('404')
- 
-})
-
-server.listen(3000,()=>{
-    console.log('Server is running on port 3000');          
-})
+const lodash = require('lodash');
+const express = require('express');
