ce862904de4a69efabb51184aeea4bb4b8410ef0
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-26 13:37:32 +0300

    .

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,9 +3,19 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  
+    res.status(200).send('Home page!')
 })
 
+app.get('/about', (req, res) => {
+   
+    res.status(200).send('About page!')
+})
+
+
+app.all('*', (req, res) => {
+    res.status(404).send('404 not found')
+})
 
 
 
