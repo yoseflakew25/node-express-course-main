f749013db08672343bab07f388410310789dc7bc
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-26 14:31:55 +0300

    .

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,24 +1,15 @@
 const express = require('express')
-const path=require('path')
 const app = express()
 const port = 3000
 
 
-app.use(express.static('public'))
-
+const {products}=require('./data')
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+    res.json(products) 
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
 })
 
 
-
-app.all('*', (req, res) => {
-    res.status(404).send('404 Not Found')
-})
-
-
-
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
