7f4d8c9bef5a40d98e04870f144007b6443721ed
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-27 02:25:54 +0300

    .

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+hconst express = require('express')
 const app = express()
 const port = 3000
 
@@ -6,7 +6,7 @@ const port = 3000
 const {products}=require('./data')
 
 app.get('/', (req, res) => {
-    res.json(products) 
+    res.json(  '<h1>Home Page</h1><a href="/api/products">products</a>') 
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
