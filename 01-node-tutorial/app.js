255bce1570987f13f643fdaadc481792ab081d5d
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-26 13:10:52 +0300

    .

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,1 +1,15 @@
-const lodash = require('lodash');
+const eventEmitter = require('events')
+
+
+
+const customEmitter = new eventEmitter()
+
+
+
+customEmitter.on('response',() => {
+    console.log('data recieved');
+})
+
+
+
+customEmitter.emit('response')
