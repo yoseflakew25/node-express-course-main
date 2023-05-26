efe811a85c84eca555e5ed827f0d6e2ffc86f9af
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-26 13:59:25 +0300

    .

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,6 +3,10 @@ const path=require('path')
 const app = express()
 const port = 3000
 
+
+app.use(express.static('public'))
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
 })
