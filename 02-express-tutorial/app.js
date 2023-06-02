e601bd5d3a491a419dbefd477835e320f17c6072
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-06-02 23:39:52 +0300

    .

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -18,10 +18,16 @@ app.get('/api/people', (req, res) => {
 
 
 
-app.post('/api/people', (req, res) => {
-    res.status(201).send("success")
-})
 
+app.post('/api/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'please provide name value' })
+  }
+  res.status(201).json({ success: true, person: name })
+})
 
 
 
