b9d142eccb86823ae38abd23967a063f743aef85
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-06-02 23:30:12 +0300

    .

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -15,7 +15,7 @@ app.get('/api/people', (req, res) => {
 
 
 app.post('/api/people', (req, res) => {
-    res.status(201).json({success:true, data:req.body})
+    res.status(201).send("success")
 })
 
 
