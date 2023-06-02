27f99d176ace45ea8e72315b83a09904518a964c
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-06-02 10:44:16 +0300

    .

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,6 +3,10 @@ const app = express()
 const port = 3000
 const {people}=require('./data')
 
+app.use(express.static('./methods-public'))
+
+
+
 app.get('/api/people', (req, res) => {
     res.status(200).json({success:true, data:people})
 })
