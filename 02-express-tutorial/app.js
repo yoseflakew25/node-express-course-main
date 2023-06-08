b2926857b5816801538f85182e1c1c10e2e3dac1
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-06-08 21:36:22 +0300

    .

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -9,6 +9,16 @@ app.use(express.urlencoded({ extended: false }))
 // parse json
 app.use(express.json())
 
+app.post('/login', (req, res) => {
+  const { name } = req.body
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+
+  res.status(401).send('Please Provide Credentials')
+})
+
+
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people })
 })
