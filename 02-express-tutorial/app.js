88eba9e5b2fd3a65568ed6a42515333822b83405
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-06-08 21:24:44 +0300

    .

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
-const {people}=require('./data')
+let { people } = require('./data')
 
 // static assets
 app.use(express.static('./methods-public'))
@@ -42,31 +41,40 @@ app.post('/login', (req, res) => {
 
   res.status(401).send('Please Provide Credentials')
 })
+
 app.put('/api/people/:id', (req, res) => {
   const { id } = req.params
   const { name } = req.body
 
-  const person = people.find((person) => {
-    return person.id === Number(id)
-  })
+  const person = people.find((person) => person.id === Number(id))
 
   if (!person) {
     return res
-      .status(400)
+      .status(404)
       .json({ success: false, msg: `no person with id ${id}` })
   }
-
-
-  const newpeople=people.map((person)=>{
+  const newPeople = people.map((person) => {
     if (person.id === Number(id)) {
       person.name = name
     }
     return person
   })
-  res.status(200).json({ success: true, data: newpeople })
-
+  res.status(200).json({ success: true, data: newPeople })
 })
 
+app.delete('/api/people/:id', (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id))
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` })
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id)
+  )
+  return res.status(200).json({ success: true, data: newPeople })
+})
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
+})
